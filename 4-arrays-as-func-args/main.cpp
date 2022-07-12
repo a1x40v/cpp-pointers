7b93e08db629d49ebfd265492d3f9f0d77cdb0cc
Alexandr <a1x40v@gmail.com>
2022-07-12 22:15:53 +0300

    The problem with array's size in functions

--- 4-arrays-as-func-args/main.cpp
+++ 4-arrays-as-func-args/main.cpp
@@ -1,8 +1,10 @@
 #include <istream>
 
-int sum_of_elements(int A[], int size)
+int sum_of_elements(int A[])
 {
   int sum = 0;
+  int size = sizeof(A) / sizeof(A[0]);
+  std::printf("SOE - Size of A = %lu, size of A[0] = %lu\n", sizeof(A), sizeof(A[0]));
   for (int i = 0; i < size; i++)
     sum += A[i];
   return sum;
@@ -11,10 +13,11 @@ int sum_of_elements(int A[], int size)
 int main()
 {
   int A[] = {1, 2, 3, 4, 5};
-  int size = sizeof(A) / sizeof(A[0]);
-  int total = sum_of_elements(A, size);
+
+  int total = sum_of_elements(A);
 
   std::printf("Sum of elements = %d\n", total);
+  std::printf("Main - Size of A = %lu, size of A[0] = %lu\n", sizeof(A), sizeof(A[0]));
 
   return 0;
 }
