7c2be85a9c3e1bd42845ce205b6e6143539cbe07
Alexandr <a1x40v@gmail.com>
2022-07-12 22:28:02 +0300

    Change arr elements inside a function

--- 4-arrays-as-func-args/main.cpp
+++ 4-arrays-as-func-args/main.cpp
@@ -1,4 +1,4 @@
-#include <istream>
+#include <iostream>
 
 int sum_of_elements(int A[], int size) // "int* A" or "int A[]" ..it's the same..
 {
@@ -9,14 +9,23 @@ int sum_of_elements(int A[], int size) // "int* A" or "int A[]" ..it's the same.
   return sum;
 }
 
+void double_elements(int *A, int size)
+{
+  for (int i = 0; i < size; i++)
+    A[i] = 2 * A[i];
+}
+
 int main()
 {
   int A[] = {1, 2, 3, 4, 5};
   int size = sizeof(A) / sizeof(A[0]);
-  int total = sum_of_elements(A, size); // A can be used for &A[0]
 
-  std::printf("Sum of elements = %d\n", total);
-  std::printf("Main - Size of A = %lu, size of A[0] = %lu\n", sizeof(A), sizeof(A[0]));
+  double_elements(A, size);
+
+  for (int i = 0; i < size; i++)
+    std::printf("%d ", A[i]);
+
+  std::cout << std::endl;
 
   return 0;
 }
