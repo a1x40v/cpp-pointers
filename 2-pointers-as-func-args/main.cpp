500c357bc5fe32135ef61f2195624a345646ece9
a1x40v <a1x40v@gmail.com>
2022-07-12 15:36:37 +0300

    Pointers as func args (lesson5)

---
+++ 2-pointers-as-func-args/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void increment(int *p)
+{
+  *p = *p + 1;
+}
+
+int main()
+{
+  int a = 10;
+
+  increment(&a);
+
+  cout << a << endl;
+
+  return 0;
+}
