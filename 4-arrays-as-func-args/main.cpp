931462e2558c65507362dff0b6b83a7a43a44064
Alexandr <a1x40v@gmail.com>
2022-07-12 22:21:21 +0300

    Solve the problem

--- 4-arrays-as-func-args/main.cpp
+++ 4-arrays-as-func-args/main.cpp
@@ -1,20 +1,19 @@
 #include <istream>
 
-int sum_of_elements(int A[])
+int sum_of_elements(int A[], int size) // "int* A" or "int A[]" ..it's the same..
 {
   int sum = 0;
-  int size = sizeof(A) / sizeof(A[0]);
   std::printf("SOE - Size of A = %lu, size of A[0] = %lu\n", sizeof(A), sizeof(A[0]));
   for (int i = 0; i < size; i++)
-    sum += A[i];
+    sum += A[i]; // A[i] is *(A+i)
   return sum;
 }
 
 int main()
 {
   int A[] = {1, 2, 3, 4, 5};
-
-  int total = sum_of_elements(A);
+  int size = sizeof(A) / sizeof(A[0]);
+  int total = sum_of_elements(A, size); // A can be used for &A[0]
 
   std::printf("Sum of elements = %d\n", total);
   std::printf("Main - Size of A = %lu, size of A[0] = %lu\n", sizeof(A), sizeof(A[0]));
